c537c4fcc34cd1fda0daf4d8ab678f9f5e81d785
suzukiplan <suzukiplan.ys@gmail.com>
2023-06-23 17:32:15 +0900

    support real joypad (wip)

--- msx2-android/app/src/main/java/com/suzukiplan/msx2_android/MainActivity.kt
+++ msx2-android/app/src/main/java/com/suzukiplan/msx2_android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.suzukiplan.msx2_android
 
 import android.content.res.AssetManager
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
@@ -15,6 +16,16 @@ class MainActivity : AppCompatActivity(), EmulatorView.Delegate {
     }
 
     private lateinit var virtualJoyPad: VirtualJoyPad
+    private val joyPad = InputStatus(
+        up = false,
+        down = false,
+        left = false,
+        right = false,
+        a = false,
+        b = false,
+        select = false,
+        start = false
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +37,32 @@ class MainActivity : AppCompatActivity(), EmulatorView.Delegate {
         findViewById<EmulatorView>(R.id.emulator).delegate = this
     }
 
+    override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
+        event ?: return false
+        val keyCode = event.keyCode
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                finish()
+            }
+            return true
+        }
+        val on = event.action == MotionEvent.ACTION_DOWN
+        when (keyCode) {
+            KeyEvent.KEYCODE_BUTTON_A -> joyPad.a = if (joyPad.a) false else on
+            KeyEvent.KEYCODE_BUTTON_B -> joyPad.b = if (joyPad.b) false else on
+            KeyEvent.KEYCODE_BUTTON_SELECT -> joyPad.select = if (joyPad.select) false else on
+            KeyEvent.KEYCODE_BUTTON_START -> joyPad.start = if (joyPad.start) false else on
+            KeyEvent.KEYCODE_DPAD_UP -> joyPad.up = if (joyPad.up) false else on
+            KeyEvent.KEYCODE_DPAD_DOWN -> joyPad.down = if (joyPad.down) false else on
+            KeyEvent.KEYCODE_DPAD_LEFT -> joyPad.left = if (joyPad.left) false else on
+            KeyEvent.KEYCODE_DPAD_RIGHT -> joyPad.right = if (joyPad.right) false else on
+            else -> return super.dispatchKeyEvent(event)
+        }
+        // TODO: 物理ボタンのキー変化をバーチャルパッドの見た目に反映した方が良いかも
+        return true
+    }
+
     override fun dispatchTouchEvent(ev: MotionEvent?) = super.dispatchTouchEvent(ev)
     override fun emulatorViewRequireAssets(): AssetManager = assets
-    override fun emulatorViewRequirePadCode() = virtualJoyPad.code
+    override fun emulatorViewRequirePadCode() = virtualJoyPad.code.or(joyPad.code)
 }
