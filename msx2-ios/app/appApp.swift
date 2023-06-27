5d2ac090d0c9316c9231f59a1942ee4e7e6a2dab
suzukiplan <suzukiplan.ys@gmail.com>
2023-06-27 20:35:06 +0900

    empty framework (obj-c) and app (swift-ui)

---
+++ msx2-ios/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Yoji Suzuki on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ msx2-ios/msx2/msx2.h
@@ -0,0 +1,18 @@
+//
+//  msx2.h
+//  msx2
+//
+//  Created by Yoji Suzuki on 2023/06/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for msx2.
+FOUNDATION_EXPORT double msx2VersionNumber;
+
+//! Project version string for msx2.
+FOUNDATION_EXPORT const unsigned char msx2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <msx2/PublicHeader.h>
+
+
