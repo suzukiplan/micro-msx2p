9b6e78cd749e80efe1f5455382cdde431f89acaa
suzukiplan <suzukiplan.ys@gmail.com>
2023-06-30 17:05:52 +0900

    split bitmap procedure to SimpleBitmap class

--- msx2-dotnet/Test/Program.cs
+++ msx2-dotnet/Test/Program.cs
@@ -30,20 +30,6 @@ using MSX2;
 
 namespace Test
 {
-    struct BitmapHeader {
-        public int isize;
-        public int width;
-        public int height;
-        public ushort planes;
-        public ushort bits;
-        public uint ctype;
-        public uint gsize;
-        public int xppm;
-        public int yppm;
-        public uint cnum;
-        public uint inum;
-    }
-
     class Program
     {
         static void Main(string[] args)
@@ -93,62 +79,17 @@ namespace Test
             ushort[] display = MSX2.Core.GetDisplay(context);
 
             Console.WriteLine("Convert display to Bitmap");
-            BitmapHeader header = new BitmapHeader();
-            header.isize = 40;
-            header.width = width;
-            header.height = height * 2;
-            header.planes = 1;
-            header.bits = 32;
-            header.ctype = 0;
-            header.gsize = (uint)(header.width * header.height * 4);
-            header.xppm = 1;
-            header.yppm = 1;
-            header.cnum = 0;
-            header.inum = 0;
-            byte[] bitmap = new byte[header.gsize];
+            SimpleBitmap bitmap = new SimpleBitmap(width, height * 2);
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
                     ushort rgb565 = display[(height - y - 1) * width + x];
-                    byte r = (byte)((rgb565 >> 11) & 0x1F);
-                    byte g = (byte)((rgb565 >> 5) & 0x3F);
-                    byte b = (byte)(rgb565 & 0x1F);
-                    r = (byte)((r << 3) | (r >> 2));
-                    g = (byte)((g << 2) | (g >> 4));
-                    b = (byte)((b << 3) | (b >> 2));
-                    bitmap[y * 2 * width * 4 + x * 4 + 0] = b;
-                    bitmap[y * 2 * width * 4 + x * 4 + 1] = g;
-                    bitmap[y * 2 * width * 4 + x * 4 + 2] = r;
-                    bitmap[y * 2 * width * 4 + x * 4 + 3] = 0;
-                    bitmap[(y * 2 + 1) * width * 4 + x * 4 + 0] = b;
-                    bitmap[(y * 2 + 1) * width * 4 + x * 4 + 1] = g;
-                    bitmap[(y * 2 + 1) * width * 4 + x * 4 + 2] = r;
-                    bitmap[(y * 2 + 1) * width * 4 + x * 4 + 3] = 0;
+                    bitmap.SetPixelRGB565(x, y * 2, rgb565);
+                    bitmap.SetPixelRGB565(x, y * 2 + 1, rgb565);
                 }
             }
 
             Console.WriteLine("Writing result.bmp");
-            var writer = new BinaryWriter(new FileStream("result.bmp", FileMode.Create));
-            try {
-                writer.Write((byte)'B');
-                writer.Write((byte)'M');
-                writer.Write((int)(14 + header.isize + header.gsize));
-                writer.Write(0);
-                writer.Write(14 + header.isize);
-                writer.Write(header.isize);
-                writer.Write(header.width);
-                writer.Write(header.height);
-                writer.Write(header.planes);
-                writer.Write(header.bits);
-                writer.Write(header.ctype);
-                writer.Write(header.gsize);
-                writer.Write(header.xppm);
-                writer.Write(header.yppm);
-                writer.Write(header.cnum);
-                writer.Write(header.inum);
-                writer.Write(bitmap, 0, (int)header.gsize);
-            } finally {
-                writer.Close();
-            }
+            bitmap.WriteFile("result.bmp");
 
             Console.WriteLine("Release micro-msx2p context");
             MSX2.Core.ReleaseContext(context);
---
+++ msx2-dotnet/Test/SimpleBitmap.cs
@@ -0,0 +1,103 @@
+/**
+ * micro MSX2+ - SimpleBitmap class
+ * -----------------------------------------------------------------------------
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023 Yoji Suzuki.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * -----------------------------------------------------------------------------
+ */
+using System;
+using System.IO;
+
+namespace Test {
+    struct BitmapHeader {
+        public int isize;
+        public int width;
+        public int height;
+        public ushort planes;
+        public ushort bits;
+        public uint ctype;
+        public uint gsize;
+        public int xppm;
+        public int yppm;
+        public uint cnum;
+        public uint inum;
+    }
+
+    class SimpleBitmap {
+        private BitmapHeader header = new BitmapHeader();
+        private byte[] bitmap = null;
+
+        public SimpleBitmap(int width, int height) {
+            header.isize = 40;
+            header.width = width;
+            header.height = height * 2;
+            header.planes = 1;
+            header.bits = 32;
+            header.ctype = 0;
+            header.gsize = (uint)(header.width * header.height * 4);
+            header.xppm = 1;
+            header.yppm = 1;
+            header.cnum = 0;
+            header.inum = 0;
+            bitmap = new byte[header.gsize];
+        }
+
+        public void SetPixelRGB565(int x, int y, ushort rgb565) {
+            byte r = (byte)((rgb565 >> 11) & 0x1F);
+            byte g = (byte)((rgb565 >> 5) & 0x3F);
+            byte b = (byte)(rgb565 & 0x1F);
+            r = (byte)((r << 3) | (r >> 2));
+            g = (byte)((g << 2) | (g >> 4));
+            b = (byte)((b << 3) | (b >> 2));
+            int ptr = y * 2 * header.width * 4 + x * 4;
+            bitmap[ptr + 0] = b;
+            bitmap[ptr + 1] = g;
+            bitmap[ptr + 2] = r;
+            bitmap[ptr + 3] = 0;
+        }
+
+        public void WriteFile(string path) {
+            var writer = new BinaryWriter(new FileStream(path, FileMode.Create));
+            try {
+                writer.Write((byte)'B');
+                writer.Write((byte)'M');
+                writer.Write((int)(14 + header.isize + header.gsize));
+                writer.Write(0);
+                writer.Write(14 + header.isize);
+                writer.Write(header.isize);
+                writer.Write(header.width);
+                writer.Write(header.height);
+                writer.Write(header.planes);
+                writer.Write(header.bits);
+                writer.Write(header.ctype);
+                writer.Write(header.gsize);
+                writer.Write(header.xppm);
+                writer.Write(header.yppm);
+                writer.Write(header.cnum);
+                writer.Write(header.inum);
+                writer.Write(bitmap, 0, (int)header.gsize);
+            } finally {
+                writer.Close();
+            }
+        }
+    }
+}
