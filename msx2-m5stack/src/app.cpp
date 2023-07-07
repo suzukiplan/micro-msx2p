cd64faaf762b03d9b89dc81ae92d6ad56aeaaf95
suzukiplan <suzukiplan.ys@gmail.com>
2023-07-08 08:21:17 +0900

    make msx2 instance dynamic

--- msx2-m5stack/src/app.cpp
+++ msx2-m5stack/src/app.cpp
@@ -1,11 +1,12 @@
 #include "msx2.hpp"
 #include <M5Core2.h>
 
-static MSX2 msx2(MSX2_COLOR_MODE_RGB555);
+static MSX2* msx2;
 
 void setup() {
-	M5.begin();
+    M5.begin();
     Serial.begin(115200);
+    msx2 = new MSX2(MSX2_COLOR_MODE_RGB555);
 }
 
 void loop() {
