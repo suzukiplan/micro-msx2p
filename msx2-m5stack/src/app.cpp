876f65b41fe9b6a0e9f5df97d2ff2ecae0193b63
suzukiplan <suzukiplan.ys@gmail.com>
2023-07-08 15:19:09 +0900

    split ticker thread (cpuId:1)

--- msx2-m5stack/src/app.cpp
+++ msx2-m5stack/src/app.cpp
@@ -1,9 +1,10 @@
+#include <mutex>
 #include "msx2.hpp"
 #include <M5Core2.h>
-#include <map>
 
 static MSX2* msx2;
-
+static std::mutex mutex;
+static bool booted;
 static struct Roms {
     uint8_t* main;
     uint8_t* logo;
@@ -15,7 +16,7 @@ static struct Roms {
     int gameSize;
 } roms;
 
-static void putlog(const char* format, ...)
+static void bootMessage(const char* format, ...)
 {
     char buf[256];
     va_list args;
@@ -23,7 +24,9 @@ static void putlog(const char* format, ...)
     vsnprintf(buf, sizeof(buf), format, args);
     va_end(args);
     Serial.println(buf);
-    M5.Lcd.println(buf); // いちいちscreenコマンドでチェックするのが面倒なので暫定的にLCDにデバッグ表示しておく
+    if (!booted) {
+        M5.Lcd.println(buf); // いちいちscreenコマンドでチェックするのが面倒なので暫定的にLCDにデバッグ表示しておく
+    }
 }
 
 static uint8_t* readRom(const char* path, int* size)
@@ -31,7 +34,7 @@ static uint8_t* readRom(const char* path, int* size)
     File file = SPIFFS.open(path);
     if (!file) return nullptr;
     *size = file.available();
-    putlog("Read %s (%d bytes)", path, *size);
+    bootMessage("Read %s (%d bytes)", path, *size);
     uint8_t* result = (uint8_t*)malloc(*size);
     if (!result) return nullptr;
     for (int i = 0; i < *size; i++) {
@@ -40,11 +43,26 @@ static uint8_t* readRom(const char* path, int* size)
     return result;    
 }
 
+void ticker(void* arg)
+{
+    unsigned long start;
+    int procTime;
+    while (1) {
+        start = millis();
+        mutex.lock();
+        msx2->tick(0, 0, 0);
+        mutex.unlock();
+        procTime = (int)(millis() - start);
+        bootMessage("%d", procTime);
+        vTaskDelay(10);
+    }
+}
+
 void setup() {
     M5.begin();
     SPIFFS.begin();
     Serial.begin(115200);
-    putlog("Loading micro MSX2+ for M5Stack...");
+    bootMessage("Loading micro MSX2+ for M5Stack...");
     msx2 = new MSX2(MSX2_COLOR_MODE_RGB565);
     roms.main = readRom("/cbios_main_msx2+_jp.rom", &roms.mainSize);
     roms.logo = readRom("/cbios_logo_msx2+.rom", &roms.logoSize);
@@ -56,17 +74,27 @@ void setup() {
     msx2->setupRAM(3, 3);
     roms.game = readRom("/game.rom", &roms.gameSize);
     msx2->loadRom(roms.game, roms.gameSize, MSX2_ROM_TYPE_NORMAL);
-    putlog("Setup finished.");
-    vTaskDelay(1000 / portTICK_RATE_MS);
+    bootMessage("Setup finished.");
+    booted = true;
+    usleep(1000000);
     M5.Lcd.clear(0);
+    xTaskCreatePinnedToCore(ticker, "ticker", 4096, nullptr, 1, nullptr, 1);
 }
 
 void loop() {
-    msx2->tick(0, 0, 0);
-    M5.Lcd.drawBitmap(
-        (M5.Lcd.width() - msx2->getDisplayWidth()) / 2,
-        (M5.Lcd.height() - msx2->getDisplayHeight()) / 2,
-        msx2->getDisplayWidth(),
-        msx2->getDisplayHeight(),
-        msx2->getDisplay());
+    static const int m5w = M5.Lcd.width();
+    static const int m5h = M5.Lcd.height();
+    static const int m2w = msx2->getDisplayWidth();
+    static const int m2h = msx2->getDisplayHeight();
+    static const int cx = (m5w - m2w) / 2;
+    static const int cy = (m5h - m2h) / 2;
+    static const size_t displaySize = m2w * m2h * 2;
+    static uint16_t* display = nullptr;
+    if (!display) {
+        display = (uint16_t*)malloc(displaySize);
+    }
+    mutex.lock();
+    memcpy(display, msx2->getDisplay(), displaySize);
+    mutex.unlock();
+    M5.Lcd.drawBitmap(cx, cy, m2w, m2h, display);
 }
