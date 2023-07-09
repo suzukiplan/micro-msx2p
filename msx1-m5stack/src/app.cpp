47d9e6dccede90612dba1dc691c5774a278ef488
suzukiplan <suzukiplan.ys@gmail.com>
2023-07-09 16:07:15 +0900

    MSX2+ -> MSX1

--- msx1-m5stack/src/app.cpp
+++ msx1-m5stack/src/app.cpp
@@ -1,18 +1,16 @@
 #include <mutex>
-#include "msx2.hpp"
+#include "msx1.hpp"
 #include <M5Core2.h>
 
-static MSX2* msx2;
+static MSX1* msx1;
 static std::mutex mutex;
 static bool booted;
 static struct Roms {
     uint8_t* main;
     uint8_t* logo;
-    uint8_t* sub;
     uint8_t* game;
     int mainSize;
     int logoSize;
-    int subSize;
     int gameSize;
 } roms;
 
@@ -52,9 +50,9 @@ void ticker(void* arg)
     while (1) {
         start = millis();
         mutex.lock();
-        msx2->tick(0, 0, 0);
+        msx1->tick(0, 0, 0);
         mutex.unlock();
-        soundData = msx2->getSound(&soundSize);
+        soundData = msx1->getSound(&soundSize);
         procTime = (int)(millis() - start);
         bootMessage("%d", procTime);
         vTaskDelay(10);
@@ -65,27 +63,29 @@ void renderer(void* arg)
 {
     const uint16_t m5w = M5.Lcd.width();
     const uint16_t m5h = M5.Lcd.height();
-    const uint16_t m2w = (uint16_t)msx2->getDisplayWidth();
-    const uint16_t m2h = (uint16_t)msx2->getDisplayHeight();
-    const uint16_t cx = (uint16_t)((m5w - m2w) / 2);
-    const uint16_t cy = (uint16_t)((m5h - m2h) / 2);
-    const size_t bitmapSize = m2w * m2h * 2;
+    const uint16_t m1w = (uint16_t)msx1->getDisplayWidth();
+    const uint16_t m1h = (uint16_t)msx1->getDisplayHeight();
+    const uint16_t cx = (uint16_t)((m5w - m1w) / 2);
+    const uint16_t cy = (uint16_t)((m5h - m1h) / 2);
+    const size_t bitmapSize = m1w * m1h * 2;
     uint16_t* bitmap = (uint16_t*)malloc(bitmapSize);
     uint16_t backdrop;
     uint16_t backdropPrev = 0;
 
     while (1) {
         mutex.lock();
-        memcpy(bitmap, msx2->getDisplay(), bitmapSize);
-        backdrop = msx2->getBackdropColor();
+        memcpy(bitmap, msx1->getDisplay(), bitmapSize);
+        backdrop = msx1->getBackdropColor();
         mutex.unlock();
         M5.Lcd.startWrite();
         if (backdrop != backdropPrev) {
             M5.Lcd.fillRect(0, 0, cx, m5h, backdrop);
-            M5.Lcd.fillRect(cx + m2w, 0, cx, m5h, backdrop);
+            M5.Lcd.fillRect(cx + m1w, 0, cx, m5h, backdrop);
+            M5.Lcd.fillRect(cx, 0, m1w, cy, backdrop);
+            M5.Lcd.fillRect(cx, cx + m1h, m1w, cy, backdrop);
             backdropPrev = backdrop;
         }
-        M5.Lcd.drawBitmap(cx, cy, m2w, m2h, bitmap);
+        M5.Lcd.drawBitmap(cx, cy, m1w, m1h, bitmap);
         M5.Lcd.endWrite();
     }
 }
@@ -94,18 +94,14 @@ void setup() {
     M5.begin();
     SPIFFS.begin();
     Serial.begin(115200);
-    bootMessage("Loading micro MSX2+ for M5Stack...");
-    msx2 = new MSX2(MSX2_COLOR_MODE_RGB565);
-    roms.main = readRom("/cbios_main_msx2+_jp.rom", &roms.mainSize);
-    roms.logo = readRom("/cbios_logo_msx2+.rom", &roms.logoSize);
-    roms.sub = readRom("/cbios_sub.rom", &roms.subSize);
-    msx2->setupSecondaryExist(false, false, false, true);
-    msx2->setup(0, 0, 0, roms.main, roms.mainSize, "MAIN");
-    msx2->setup(0, 0, 4, roms.logo, roms.logoSize, "LOGO");
-    msx2->setup(3, 0, 0, roms.sub, roms.subSize, "SUB");
-    msx2->setupRAM(3, 3);
+    bootMessage("Loading micro msx1+ for M5Stack...");
+    msx1 = new MSX1(MSX1_COLOR_MODE_RGB565);
+    roms.main = readRom("/cbios_main_msx1.rom", &roms.mainSize);
+    roms.logo = readRom("/cbios_logo_msx1", &roms.logoSize);
+    msx1->setup(0, 0, roms.main, roms.mainSize, "MAIN");
+    msx1->setup(0, 4, roms.logo, roms.logoSize, "LOGO");
     roms.game = readRom("/game.rom", &roms.gameSize);
-    msx2->loadRom(roms.game, roms.gameSize, MSX2_ROM_TYPE_NORMAL);
+    msx1->loadRom(roms.game, roms.gameSize, MSX1_ROM_TYPE_NORMAL);
     bootMessage("Setup finished.");
     booted = true;
     usleep(1000000);
