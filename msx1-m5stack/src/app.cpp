800bb489833926bed996bc4d16ab1b27872ba0df
suzukiplan <suzukiplan.ys@gmail.com>
2023-07-21 20:44:56 +0900

    AW88298

--- msx1-m5stack/src/app.cpp
+++ msx1-m5stack/src/app.cpp
@@ -36,15 +36,16 @@ class CustomCanvas : public lgfx::LGFX_Sprite {
 
 class Audio {
 #ifdef M5StackCore2
+    // Simple Audio DAC implementation
   private:
-    const i2s_port_t i2sNum = I2S_NUM_0;
+    static constexpr i2s_port_t i2sNum = I2S_NUM_0;
 
   public:
     void begin() {
         i2s_config_t audioConfig = {
             .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
             .sample_rate = 44100,
-            .bits_per_sample = I2S_BITS_PER_SAMPLE_8BIT,
+            .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
             .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
             .communication_format = I2S_COMM_FORMAT_STAND_MSB,
             .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
@@ -54,28 +55,66 @@ class Audio {
             .tx_desc_auto_clear = true
         };
         i2s_driver_install(this->i2sNum, &audioConfig, 0, nullptr);
-        i2s_set_clk(this->i2sNum, 44100, I2S_BITS_PER_SAMPLE_8BIT, I2S_CHANNEL_MONO);
+        i2s_set_clk(this->i2sNum, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
         i2s_zero_dma_buffer(this->i2sNum);
     }
 
-    inline void write(int8_t* buf, size_t bufSize) {
+    inline void write(int16_t* buf, size_t bufSize) {
         size_t wrote;
         i2s_write(this->i2sNum, buf, bufSize, &wrote, portMAX_DELAY);
         vTaskDelay(2);
     }
 #else
-  public:
-    void begin() {
-        auto config = M5.Speaker.config();
-        config.sample_rate = 44100;
-        M5.Speaker.config(config);
-        M5.Speaker.setVolume(255);
-        M5.Speaker.begin();
+    // AW88298 Audio Amplifier implementation
+  private:
+    static constexpr uint8_t i2cAddrAw88298 = 0x36;
+    static constexpr i2s_port_t i2sNum = I2S_NUM_1;
+
+    inline void writeRegister(uint8_t reg, uint16_t value) {
+        value = __builtin_bswap16(value);
+        M5.In_I2C.writeRegister(this->i2cAddrAw88298, reg, (const uint8_t*)&value, 2, 400000);
     }
 
-    inline void write(int8_t* buf, size_t bufSize) {
-        while (M5.Speaker.isPlaying()) { vTaskDelay(1); }
-        M5.Speaker.playRaw(buf, bufSize, 44100, false);
+  public:
+    void begin() {
+        // setup AW88298 regisgter
+        M5.In_I2C.bitOn(this->i2cAddrAw88298, 0x02, 0b00000100, 400000);
+        this->writeRegister(0x61, 0x0673); // BSTCTL2 (same as M5Unified) 
+        this->writeRegister(0x04, 0x4040); // SYSCTL (same as M5Unified)
+        this->writeRegister(0x05, 0x0008); // SYSCTL2 (same as M5Unified)
+        this->writeRegister(0x06, 0b0001110000000111); // I2SCTL: 44.1kHz, 16bits, monoral (他は全部デフォルト値)
+        this->writeRegister(0x0C, 0x0064); // HAGCCFG (same as M5Unified)
+        // I2S config
+        i2s_config_t config;
+        memset(&config, 0, sizeof(i2s_config_t));
+        config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX);
+        config.sample_rate = 48000; // dummy setting
+        config.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT;
+        config.channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT;
+        config.communication_format = I2S_COMM_FORMAT_STAND_I2S;
+        config.dma_buf_count = 4;
+        config.dma_buf_len = 1024;
+        config.tx_desc_auto_clear = true;
+        // I2S pin config
+        i2s_pin_config_t pinConfig;
+        memset(&pinConfig, ~0u, sizeof(i2s_pin_config_t));
+        pinConfig.bck_io_num = GPIO_NUM_34;
+        pinConfig.ws_io_num = GPIO_NUM_33;
+        pinConfig.data_out_num = GPIO_NUM_13;
+        // Setup I2S
+        if (ESP_OK != i2s_driver_install(this->i2sNum, &config, 0, nullptr)) {
+            i2s_driver_uninstall(this->i2sNum);
+            i2s_driver_install(this->i2sNum, &config, 0, nullptr);
+        }
+        i2s_set_pin(this->i2sNum, &pinConfig);
+        i2s_zero_dma_buffer(this->i2sNum);
+        i2s_start(this->i2sNum);
+    }
+
+    inline void write(int16_t* buf, size_t bufSize) {
+        size_t wrote;
+        i2s_write(this->i2sNum, buf, bufSize, &wrote, portMAX_DELAY);
+        vTaskDelay(2);
     }
 #endif
 };
@@ -541,7 +580,7 @@ void IRAM_ATTR ticker(void* arg)
 
 void IRAM_ATTR psgTicker(void* arg)
 {
-    static int8_t buf[1024];
+    static int16_t buf[512];
     static size_t size;
     static int i;
     while (1) {
@@ -553,8 +592,8 @@ void IRAM_ATTR psgTicker(void* arg)
             psgTickerPaused = false;
         }
         if (0 < pref.sound) {
-            for (i = 0; i < 1024; i++) {
-                buf[i] = psg.tick8(81);
+            for (i = 0; i < 512; i++) {
+                buf[i] = psg.tick16(81);
             }
             audio.write(buf, sizeof(buf));
         } else {
@@ -799,10 +838,6 @@ void setup() {
     pref.load();
     resetRotation();
     displayMessage("Initializing...");
-    displayMessage("Initialize I2S (Audio)");
-    audio.begin();
-    displayMessage("Initialize I2C (GamePad)");
-    gamepad.begin();
     displayMessage("Checking memory usage before launch MSX...");
     displayMessage("- HEAP: %d", esp_get_free_heap_size());
     displayMessage("- MALLOC_CAP_EXEC: %d", heap_caps_get_free_size(MALLOC_CAP_EXEC));
@@ -815,6 +850,11 @@ void setup() {
     msx1.setupKeyAssign(0, MSX1_JOY_S1, 0x20); // START = SPACE
     msx1.setupKeyAssign(0, MSX1_JOY_S2, 0x1B); // SELECT = ESC
     msx1.loadRom((void*)rom_game, sizeof(rom_game), MSX1_ROM_TYPE_NORMAL);
+    displayMessage("Initializing I2S (Audio)");
+    audio.begin();
+    displayMessage("Initializing I2C (GamePad)");
+    gamepad.begin();
+    displayMessage("Initializing PSG emulator");
     msx1.psgDelegate.reset = []() { psg.reset(27); };
     msx1.psgDelegate.setPads = [](unsigned char pad1, unsigned char pad2) { psg.setPads(pad1, pad2); };
     msx1.psgDelegate.read = []() -> unsigned char { return psg.read(); };
