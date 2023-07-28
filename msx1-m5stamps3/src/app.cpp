05d6d9a7d5d38191aa6ed9f3f7964efadb2989cc
suzukiplan <suzukiplan.ys@gmail.com>
2023-07-28 23:28:49 +0900

    implement

--- msx1-m5stamps3/src/app.cpp
+++ msx1-m5stamps3/src/app.cpp
@@ -24,22 +24,248 @@
  * THE SOFTWARE.
  * -----------------------------------------------------------------------------
  */
+// C++ stdlib
+#include <map>
+#include <string>
+#include <vector>
+
+// micro MSX2+
+#include "ay8910.hpp"
+#include "msx1.hpp"
+#include "roms.hpp"
+
+// Arduino
+#include <Arduino.h>
+#include <SD.h>
+#include <SPIFFS.h>
+#include <Wire.h>
+#include <driver/i2s.h>
+#include <esp_freertos_hooks.h>
+#include <esp_log.h>
+
+// LGFX
 #include "ILI9431.hpp"
 
-static ILI9431 lcd;
+class CustomCanvas : public lgfx::LGFX_Sprite
+{
+  public:
+    CustomCanvas() : LGFX_Sprite() {}
+
+    CustomCanvas(LovyanGFX* parent) : LGFX_Sprite(parent)
+    {
+        _psram = false; // PSRAM is not used to ensure performance (DMA is used)
+    }
+
+    void* frameBuffer(uint8_t)
+    {
+        return getBuffer();
+    }
+};
+
+static uint8_t ram[0x4000];
+static TMS9918A::Context vram;
+static ILI9431 gfx;
+static CustomCanvas canvas(&gfx);
+static uint16_t displayBuffer[256];
+static xSemaphoreHandle displayMutex;
+static unsigned short backdropColor;
+static int fps;
+static int cpu0;
+static int cpu1;
+static uint64_t idle0 = 0;
+static uint64_t idle1 = 0;
+static AY8910 psg;
+
+static void displayMessage(const char* format, ...)
+{
+    char buf[256];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buf, sizeof(buf), format, args);
+    va_end(args);
+    gfx.startWrite();
+    gfx.println(buf);
+    gfx.endWrite();
+    vTaskDelay(100);
+}
+
+static IRAM_ATTR bool idleTask0()
+{
+    idle0++;
+    return false;
+}
+
+static IRAM_ATTR bool idleTask1()
+{
+    idle1++;
+    return false;
+}
+
+static DRAM_ATTR MSX1 msx1(TMS9918A::ColorMode::RGB565_Swap, ram, sizeof(ram), &vram, [](void* arg, int frame, int lineNumber, uint16_t* display) {
+    canvas.pushImage(0, lineNumber, 256, 1, display);
+    if (191 == lineNumber) {
+        backdropColor = ((MSX1*)arg)->getBackdropColor(true);
+    }
+});
+
+void IRAM_ATTR ticker(void* arg)
+{
+    static long start;
+    static long procTime = 0;
+    static const long interval[3] = {16, 17, 17};
+    static int loopCount = 0;
+    static int fpsCounter;
+    static long sec = 0;
+    while (1) {
+        start = millis();
+
+        // calc fps
+        if (sec != start / 1000) {
+            sec = start / 1000;
+            fps = fpsCounter;
+            fpsCounter = 0;
+        }
+
+        // execute even frame (rendering display buffer)
+        xSemaphoreTake(displayMutex, portMAX_DELAY);
+        msx1.tick(0, 0, 0);
+        xSemaphoreGive(displayMutex);
+        fpsCounter++;
+
+        // wait
+        procTime = millis() - start;
+        if (procTime < interval[loopCount]) {
+            ets_delay_us((interval[loopCount] - procTime) * 1000);
+        }
+        loopCount++;
+        loopCount %= 3;
+
+        // execute odd frame (skip rendering)
+        start = millis();
+        msx1.tick(0, 0, 0);
+        fpsCounter++;
+
+        // wait
+        procTime = millis() - start;
+        if (procTime < interval[loopCount]) {
+            ets_delay_us((interval[loopCount] - procTime) * 1000);
+        }
+        loopCount++;
+        loopCount %= 3;
+    }
+}
+
+void IRAM_ATTR psgTicker(void* arg)
+{
+    static int16_t buf[512];
+    static size_t size;
+    static int i;
+    while (1) {
+        for (i = 0; i < 512; i++) {
+            buf[i] = psg.tick16(81);
+        }
+        vTaskDelay(2);
+        // audio.write(buf, sizeof(buf));
+    }
+}
+
+void renderer(void* arg)
+{
+    static uint16_t backdropPrev = 0;
+    static char buf[80];
+    static long start;
+    static long procTime;
+    static int renderFps;
+    static int renderFpsCounter;
+    static long sec;
+    while (1) {
+        start = millis();
+        if (sec != start / 1000) {
+            sec = start / 1000;
+            renderFps = renderFpsCounter;
+            renderFpsCounter = 0;
+        }
+        gfx.startWrite();
+        if (backdropColor != backdropPrev) {
+            backdropPrev = backdropColor;
+            gfx.fillRect(0, 8, 320, 16, backdropPrev);
+            gfx.fillRect(0, 216, 320, 16, backdropPrev);
+            gfx.fillRect(0, 24, 32, 192, backdropPrev);
+            gfx.fillRect(288, 24, 32, 192, backdropPrev);
+        }
+        gfx.setCursor(0, 232);
+        sprintf(buf, "EMU:%d/60fps  LCD:%d/30fps  C0:%d%%  C1:%d%% ", fps, renderFps, cpu0, cpu1);
+        gfx.print(buf);
+        xSemaphoreTake(displayMutex, portMAX_DELAY);
+        canvas.pushSprite(32, 24);
+        xSemaphoreGive(displayMutex);
+        gfx.endWrite();
+        renderFpsCounter++;
+        procTime = millis() - start;
+        if (procTime < 33) {
+            ets_delay_us((33 - procTime) * 1000);
+        }
+    }
+}
+
+void cpuMonitor(void* arg)
+{
+    while (1) {
+        float f0 = idle0;
+        float f1 = idle1;
+        idle0 = 0;
+        idle1 = 0;
+        cpu0 = (int)(100.f - f0 / 1855000.f * 100.f);
+        cpu1 = (int)(100.f - f1 / 1855000.f * 100.f);
+        vTaskDelay(1000);
+    }
+}
 
 void setup()
 {
-    lcd.init();
-    lcd.startWrite();
-    lcd.setRotation(1);
-    lcd.writeFillRect(0, 0, lcd.width(), lcd.height(), 0x003F);
-    lcd.setTextSize(2);
-    lcd.setTextColor(0xFFFF, 0x0000);
-    lcd.setCursor(0, 0);
-    lcd.println("Hello, World!");
-    lcd.println("Using LovyanGFX.");
-    lcd.endWrite();
+    gfx.init();
+    gfx.setRotation(1);
+    gfx.setColorDepth(16);
+    gfx.fillScreen(TFT_BLACK);
+    canvas.setColorDepth(16);
+    canvas.createSprite(256, 192);
+    displayMutex = xSemaphoreCreateMutex();
+    displayMessage("Initializing...");
+    displayMessage("Checking memory usage before launch MSX...");
+    displayMessage("- HEAP: %d", esp_get_free_heap_size());
+    displayMessage("- MALLOC_CAP_EXEC: %d", heap_caps_get_free_size(MALLOC_CAP_EXEC));
+    displayMessage("- MALLOC_CAP_DMA-L: %d", heap_caps_get_largest_free_block(MALLOC_CAP_DMA));
+    displayMessage("- MALLOC_CAP_INTERNAL: %d", heap_caps_get_free_size(MALLOC_CAP_INTERNAL));
+    displayMessage("Loading micro MSX2+ (MSX1-core) for M5Stack...");
+    msx1.vdp.useOwnDisplayBuffer(displayBuffer, sizeof(displayBuffer));
+    msx1.setup(0, 0, (void*)rom_cbios_main_msx1, sizeof(rom_cbios_main_msx1), "MAIN");
+    msx1.setup(0, 4, (void*)rom_cbios_logo_msx1, sizeof(rom_cbios_logo_msx1), "LOGO");
+    msx1.setupKeyAssign(0, MSX1_JOY_S1, 0x20); // START = SPACE
+    msx1.setupKeyAssign(0, MSX1_JOY_S2, 0x1B); // SELECT = ESC
+    msx1.loadRom((void*)rom_game, sizeof(rom_game), MSX1_ROM_TYPE_NORMAL);
+    displayMessage("Initializing PSG emulator");
+    msx1.psgDelegate.reset = []() { psg.reset(27); };
+    msx1.psgDelegate.setPads = [](unsigned char pad1, unsigned char pad2) { psg.setPads(pad1, pad2); };
+    msx1.psgDelegate.read = []() -> unsigned char { return psg.read(); };
+    msx1.psgDelegate.getPad1 = []() -> unsigned char { return psg.getPad1(); };
+    msx1.psgDelegate.getPad2 = []() -> unsigned char { return psg.getPad2(); };
+    msx1.psgDelegate.latch = [](unsigned char value) { return psg.latch(value); };
+    msx1.psgDelegate.write = [](unsigned char value) { return psg.write(value); };
+    msx1.psgDelegate.getContext = []() -> const void* { return &psg.ctx; };
+    msx1.psgDelegate.getContextSize = []() -> int { return (int)sizeof(psg.ctx); };
+    msx1.psgDelegate.setContext = [](const void* context, int size) { memcpy(&psg.ctx, context, size); };
+    psg.reset(27);
+    psg.setVolume(2);
+    displayMessage("MSX1-core setup finished.");
+    usleep(1000000);
+    gfx.clear();
+    disableCore0WDT();
+    esp_register_freertos_idle_hook_for_cpu(idleTask0, 0);
+    esp_register_freertos_idle_hook_for_cpu(idleTask1, 1);
+    xTaskCreatePinnedToCore(ticker, "ticker", 4096, nullptr, 25, nullptr, 0);
+    xTaskCreatePinnedToCore(psgTicker, "psgTicker", 4096, nullptr, 25, nullptr, 1);
+    xTaskCreatePinnedToCore(renderer, "renderer", 4096, nullptr, 25, nullptr, 1);
+    xTaskCreatePinnedToCore(cpuMonitor, "cpuMonitor", 1024, nullptr, 1, nullptr, 1);
 }
 
 void loop() {}
